14a3fed661f9e4502a0462c92f7695ecc233887d
md-amir-gauhar <gauharamir15@gmail.com>
2024-11-02 23:34:30 +0530

    feat: Setting page layout

---
+++ src/app/(website)/_components/navbar.tsx
@@ -0,0 +1,18 @@
+import { MenuIcon } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+const LandingPageNavbar = () => {
+  return (
+    <div className="flex w-full justify-between items-center">
+      <div className="text-3xl font-semibold flex items-center gap-x-3">
+        <MenuIcon className="w-8 h-8" />
+        <Image src="/opal-logo.svg" alt="logo" width={40} height={40} />
+        Opal
+      </div>
+      LandingPageNavbar
+    </div>
+  );
+};
+
+export default LandingPageNavbar;
